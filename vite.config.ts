e1bffa54979094da548ce8ba43769663311d5a12
Aliyah Abdullah <aliyahadullah11@gmail.com>
2026-01-20 17:12:23 +0500

    fix vite config merge conflict

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-<<<<<<< HEAD
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/alimedtech-website/', // 👈 YE LINE ADD KI HAI (repo name)
-
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
-});
-=======
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
->>>>>>> b66dd3b2d52b639551c4c65be86f7ed4ac7d047f
+export default defineConfig({
+  plugins: [react()],
+  base: '/alimedtech-website/',
+})
